3771cd3ccf37694e6d290cd97e27854e230bee8d
Thomas Maximini <tmaximini@gmail.com>
2018-06-18 22:21:50 +0200

    post mutation, routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
 import LinkList from './components/LinkList'
+import CreateLink from './components/CreateLink'
 
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LinkList />
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 // 1
 import { ApolloProvider } from 'react-apollo'
@@ -22,9 +23,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 registerServiceWorker()
